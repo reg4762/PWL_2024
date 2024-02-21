5e21e2aaa044151258b95f4b60020ca5b860a47e
Regita Delya Putri <regitadelyap@gmail.com>
2024-02-21 16:22:58 +0700

    modifikasi ketiga

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    // public function index() {
+    //     return 'Selamat Datang';
+    // }
+
+    // public function about() {
+    //     return 'Regita Delya Putri, 2241720137';
+    // }
+
+    // public function articles($id) {
+    //     return 'Halaman Artikel dengan ID ' .$id ;
+    // }
+}
+    
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);  
+
+Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+]);
+
+Route::get('/', [HomeController::class,'index']);
+
+Route::get('/about', [AboutController::class,'about']);
+
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+// Route::get('/', [PageController::class,'index']);
+
+// Route::get('/about', [PageController::class,'about']);
+
+// Route::get('/articles/{id}', [PageController::class,'articles']);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/user/{name?}', function ($name='John') { 
     return 'Nama saya '.$name;
@@ -10,9 +38,9 @@ Route::get('/user/{name?}', function ($name='John') {
 //     return 'Nama saya '.$name;
 // });
 
-Route::get('/articles/{id} ', function ($id) { 
-    return 'Halaman Artikel dengan ID '.$id;
-});
+// Route::get('/articles/{id} ', function ($id) { 
+//     return 'Halaman Artikel dengan ID '.$id;
+// });
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
@@ -22,19 +50,19 @@ Route::get('/user/{name}', function ($name) {
     return 'Nama Saya '.$name;
 });
 
-Route::get('/', function () { 
-    return 'Selamat Datang';
-});
+// Route::get('/', function () { 
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () { 
-    return 'NIM: 2241720137, Nama:Regita Delya Putri';
-});
+// Route::get('/about', function () { 
+//     return 'NIM: 2241720137, Nama:Regita Delya Putri';
+// });
 
 Route::get('/world', function () { 
     return 'World';
 });
 
-Route::get('/hello', function () {
- return 'Hello World';
-});
+// Route::get('/hello', function () {
+//  return 'Hello World';
+// });
 
