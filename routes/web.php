b757aff18eeec7699cc250ec9b229a0ea4c6d5f8
Regita Delya Putri <regitadelyap@gmail.com>
2024-02-21 10:47:58 +0700

    modifikasi kedua

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,39 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
 });
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+
+Route::get('/articles/{id} ', function ($id) { 
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+    
+Route::get('/user/{name}', function ($name) { 
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/', function () { 
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () { 
+    return 'NIM: 2241720137, Nama:Regita Delya Putri';
+});
+
+Route::get('/world', function () { 
+    return 'World';
+});
+
+Route::get('/hello', function () {
+ return 'Hello World';
+});
+
